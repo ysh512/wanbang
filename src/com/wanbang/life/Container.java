3e23b352a342043f777f4d8ecd4abbcc0734b106
ysh <78092133@qq.com>
2015-03-09 20:55:17 +0800

    push

--- src/com/wanbang/life/Container.java
+++ src/com/wanbang/life/Container.java
@@ -7,18 +7,30 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 
 public class Container extends FragmentActivity {
 
 	private AccountFragment accountFragment;
+	private MainpageFragment mainpageFragment;
 	private Button btn_main;
 	private Button btn_indi;
 	private Button btn_more;
+	
+	
+	
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		// TODO Auto-generated method stub
+//		super.onSaveInstanceState(outState);
+	}
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		this.setContentView(R.layout.container);
 		btn_main = (Button)findViewById(R.id.btn_main);
 		btn_indi = (Button)findViewById(R.id.btn_indi);
@@ -27,6 +39,12 @@ public class Container extends FragmentActivity {
 		btn_main.setOnClickListener(fragmentClick);
 		btn_indi.setOnClickListener(fragmentClick);
 		btn_more.setOnClickListener(fragmentClick);
+		
+		mainpageFragment = new MainpageFragment();
+		FragmentManager fm = this.getSupportFragmentManager();
+		FragmentTransaction ft= fm.beginTransaction();
+		ft.add(R.id.fragment,mainpageFragment,"MainPage");
+		ft.commit();
 		
 	}
 
@@ -48,8 +66,12 @@ public class Container extends FragmentActivity {
 					accountFragment = new AccountFragment();
 				}
 				ft.replace(R.id.fragment, accountFragment);
+				ft.addToBackStack(null);
 				ft.commit();
 				break;
+			case R.id.btn_main:
+				ft.replace(R.id.fragment, mainpageFragment);
+				ft.commit();
 			}
 		}
 	};
--- src/com/wanbang/life/MainActivity.java
+++ src/com/wanbang/life/MainActivity.java
@@ -1,15 +1,17 @@
 package com.wanbang.life;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 public class MainActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 	}
 
